fd801241ade5db3d834511cf5ed78abfa3978c18
Mintemir777 <mintemir2004@mail.ru>
2025-03-11 04:43:47 +0500

    add channels and new go routine

--- main.go
+++ main.go
@@ -26,14 +26,15 @@ type User struct {
 }
 
 func main() {
-	time.Sleep(time.Millisecond * 30)
+	time.Sleep(time.Millisecond * 10)
 	t := time.Now()
 
 	wg := &sync.WaitGroup{}
 	rand.Seed(time.Now().UTC().UnixNano())
 
-	users := generateUsers(1500)
-	for _, user := range users {
+	users := make(chan User)
+	go generateUsers(1000, users)
+	for user := range users {
 		wg.Add(1)
 		go saveUserInfo(user, wg)
 	}
@@ -50,19 +51,18 @@ func (u User) getActivityInfo() string {
 	return out
 }
 
-func generateUsers(count int) []User {
-	users := make([]User, count)
+func generateUsers(count int, users chan User) {
 	for i := 0; i < count; i++ {
-		users[i] = User{
+		users <- User{
 			id:    i + 1,
 			email: fmt.Sprintf("user%s@mail.go", i+1),
 			logs:  generateLogs(rand.Intn(200)),
 		}
 	}
-	return users
+	close(users)
 }
 func saveUserInfo(user User, wg *sync.WaitGroup) error {
-	fmt.Println("Saving user:\n", user.id)
+	fmt.Println("Saving user \n", user.id)
 	filename := fmt.Sprintf("logs/user_%d.txt", user.id)
 	file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0644)
 	if err != nil {
